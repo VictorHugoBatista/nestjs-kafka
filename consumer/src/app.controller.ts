7e78f6c49ad8e5b2cf7cf51f1d86a070ea619f9c
Victor Hugo Batista <vct.bta@gmail.com>
2021-02-08 15:01:13 -0300

    Envia as mensagens ao kafka por meio de producers

--- consumer/src/app.controller.ts
+++ consumer/src/app.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Get, Inject } from '@nestjs/common';
+import { Controller, Get, Inject, OnModuleInit } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ClientKafka, MessagePattern, Payload } from '@nestjs/microservices';
+import { Producer } from '@nestjs/microservices/external/kafka.interface';
 
 @Controller()
-export class AppController {
+export class AppController implements OnModuleInit {
+  private kaftaProducer: Producer;
+
   constructor(
     private readonly appService: AppService,
     @Inject('TEST_SERVICE') private readonly kafkaClient: ClientKafka,
@@ -13,12 +16,24 @@ export class AppController {
     // Need to subscribe to topic 
     // so that we can get the response from kafka microservice
     this.kafkaClient.subscribeToResponseOf('topic-test-return');
-    await this.kafkaClient.connect();
+    this.kaftaProducer = await this.kafkaClient.connect();
   }
 
   @Get()
   getHello(): string {
-    this.kafkaClient.emit<object>('topic-test-return', {test: 'connection'});
+    this.kaftaProducer.send({
+      topic: 'topic-test-return',
+      messages: [
+        {
+          key: Math.random().toString(),
+          value: JSON.stringify({teste: 'connect1'}),
+        },
+        {
+          key: Math.random().toString(),
+          value: JSON.stringify({teste: 'connect2'}),
+        },
+      ],
+    });
     console.log('send test connection')
     return this.appService.getHello() + ' teste consumer';
   }
@@ -26,7 +41,17 @@ export class AppController {
   @MessagePattern('topic-test') // Our topic name
   helloTopic(@Payload() message) {
     console.log('message', message.value);
-    setTimeout(() => this.kafkaClient.emit<object>('topic-test-return', {result: 5 + 5}), 2000);
+    setTimeout(() => {
+      this.kaftaProducer.send({
+        topic: 'topic-test-return',
+        messages: [
+          {
+            key: Math.random().toString(),
+            value: JSON.stringify({result: 5 + 5}),
+          },
+        ],
+      });
+    }, 2000);
     return message.value;
   }
 }
--- producer/src/app.controller.ts
+++ producer/src/app.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Get, Inject } from '@nestjs/common';
+import { Controller, Get, Inject, OnModuleInit } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ClientKafka, MessagePattern, Payload } from '@nestjs/microservices';
+import { Producer } from '@nestjs/microservices/external/kafka.interface';
 
 @Controller()
-export class AppController {
+export class AppController implements OnModuleInit {
+  private kaftaProducer: Producer;
+
   constructor(
     private readonly appService: AppService,
     @Inject('TEST_SERVICE') private readonly kafkaClient: ClientKafka,
@@ -13,12 +16,20 @@ export class AppController {
     // Need to subscribe to topic 
     // so that we can get the response from kafka microservice
     this.kafkaClient.subscribeToResponseOf('topic-test');
-    await this.kafkaClient.connect();
+    this.kaftaProducer = await this.kafkaClient.connect();
   }
 
   @Get()
   getHello(): string {
-    this.kafkaClient.emit<object>('topic-test', {number: 5});
+    this.kaftaProducer.send({
+      topic: 'topic-test',
+      messages: [
+        {
+          key: Math.random().toString(),
+          value: JSON.stringify({number: 5}),
+        },
+      ],
+    });
     console.log('send teste');
     return this.appService.getHello();
   }
